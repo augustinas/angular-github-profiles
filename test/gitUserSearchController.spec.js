8b1bb0dc6ac8c0175043257cf526f6058151f998
Joseph Knowles <joejknowles@gmail.com>
2015-04-29 16:37:09 +0100

    Added search term functionality

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -13,6 +13,17 @@ describe('GitUserSearchController', function() {
   });
 
   describe('when searching for a user', function() {
+    var httpBackend;
+
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend;
+      httpBackend
+        .when("GET", "https://api.github.com/search/users?q=hello")
+        .respond(
+          { items: items }
+        );
+    }));
+
     var items = [
       {
         "login": "tansaku",
@@ -27,8 +38,9 @@ describe('GitUserSearchController', function() {
     ];
 
     it('display search results', function() {
-      // ctrl.searchTerm = 'hello';
+      ctrl.searchTerm = 'hello';
       ctrl.doSearch();
+      httpBackend.flush();
       expect(ctrl.searchResult.items).toEqual(items);
     });
   });
