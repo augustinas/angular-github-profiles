9dfd882fe5b0b38244e2704302a1bdcd78792d73
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-04-30 15:58:13 +0100

    Trying to change ports

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var port = process.env.PORT || 5000;
 var express = require('express');
 var app = express();
 
@@ -10,7 +11,7 @@ app.get('/', function(request, response) {
   response.render('index', {name: 'You choose'});
 });
 
-app.listen(3000, function () {
-  console.log('server listening on port 3000');
+app.listen(port, function () {
+  console.log('server listening on port ' + port);
 });
 module.exports = app;
